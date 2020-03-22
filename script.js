90e2f7017932a08bd0516e32aaf73af54291adb2
pesova <pesova13@gmail.com>
2020-03-22 06:02:43 +0100

    javascript HNG

---
+++ script.js
@@ -0,0 +1,15 @@
+var name = "My Name is Pesova";
+var Courses = ["Html", "CSS", "JavaScript", "Python", "GO", "PHP", "Csharp", "Design"];
+
+console.log(name)
+console.log(Courses)
+
+
+let arr1 = Courses.length;
+
+console.log("number of courses is " + arr1)
+
+
+for (let i = 2; i < 200; i = i + 2) {
+    console.log(i)
+}
